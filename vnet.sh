551dc352cda3a999c6597fc061364b6527b0887f
adkwolek <adam.kwolek@capgemini.com>
2022-08-21 17:01:45 +0200

    update vnet

--- vnet.sh
+++ vnet.sh
@@ -7,9 +7,17 @@ address=10.0.0.0/16
 subnetArray=(10.0.0.0/25 10.0.0.128/25)
 subnetName=(ubuntu windows)
 len=${#subnetArray[@]}
-az network vnet create --location $location --name $name --resource-group $resource --address-prefix $address 
+az network vnet create\
+    --location $location\
+    --name $name\
+    --resource-group $resource\
+    --address-prefix $address 
 for (( i=0; i<$len; i++ ));
 do
-az network vnet subnet create --name ${subnetName[$i]}  --vnet-name $name --resource-group $resource  --address-prefixes ${subnetArray[$i]} 
+az network vnet subnet create\
+    --name ${subnetName[$i]}\
+    --vnet-name $name\
+    --resource-group $resource\
+    --address-prefixes ${subnetArray[$i]} 
 done
 
