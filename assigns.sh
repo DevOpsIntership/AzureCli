79128cb1a4771b93103b76f07a053b9a76340ee3
adkwolek <adam.kwolek@capgemini.com>
2022-08-22 07:43:59 +0200

    assigns

---
+++ assigns.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "assigns NSG to vnet subnet"
+nsgname=(NsgUbuntu NsgWindows)
+vnetname=Vnet
+subnetname=(ubuntu windows)
+for ((i=0; i<2; i++))
+do
+az network vnet subnet update\
+    --resource-group automation\
+    --vnet-name $vnetname\
+    --name ${subnetname[$i]}\
+    --network-security-group ${nsgname[$i]}
+done    
+    
