849df5c1841065348ede3247fe3ceb360b7a2e2b
adkwolek <110588536+adkwolek@users.noreply.github.com>
2022-08-24 16:02:32 +0200

    Add files via upload

---
+++ AppService.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Create AppService"
+
+Region=ukwest
+Name=AppServiceTest
+Pricing_tier=B1
+Nodes_in_web_farm=2
+Docker_Hub_image=docker/getting-started
+
+az appservice plan create\
+    --resource-group automationbash\
+    --name $Name\
+    --location $Region\
+    --is-linux\
+    --number-of-workers $Nodes_in_web_farm\
+    --sku $Pricing_tier\
+
+    az webapp create\
+    --resource-group automationbash\
+    --plan $Name\
+    --name adam98022test\
+    --deployment-container-image-name $Docker_Hub_image\
