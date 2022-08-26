d38de1816e2c135572f66b905ef9d79fe9cba494
adkwolek <110588536+adkwolek@users.noreply.github.com>
2022-08-26 07:10:06 +0200

    Update assigns.sh

--- assigns.sh
+++ assigns.sh
@@ -3,10 +3,11 @@ echo "assigns NSG to vnet subnet"
 nsgname=(NsgUbuntu NsgWindows)
 vnetname=Vnet
 subnetname=(ubuntu windows)
+rg=$1
 for ((i=0; i<2; i++))
 do
 az network vnet subnet update\
-    --resource-group automationbash\
+    --resource-group $rg\
     --vnet-name $vnetname\
     --name ${subnetname[$i]}\
     --network-security-group ${nsgname[$i]}
